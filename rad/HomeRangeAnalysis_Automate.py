e34b3ec5b3a02c1227b4a9c7c259b12c658f9e89
Ruben Dorador <rubendorador@gmail.com>
2023-06-30 10:55:02 -0400

    automate submission script

--- rad/HomeRangeAnalysis_Automate.py
+++ rad/HomeRangeAnalysis_Automate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from glob import glob
 import argparse
 import os
@@ -11,14 +12,19 @@ args = parser.parse_args()
 def main():
 	bashes = glob("{}/**/*sh".format(args.Data), recursive = True)
 	bashes = [os.path.abspath(path) for path in bashes]
-	template = "cd **path**\nsbatch **bashfile**cd **datapath**\n\n"
+	template = "cd **path**\nsbatch **bashfile**\ncd **datapath**\n\n\n"
 	paths = [i.rsplit("/", 1)[0] for i in bashes]
 	bashfiles = [i.rsplit("/", 1)[1] for i in bashes]
 	# print(paths)
 	# print("/n/n")
 	# print(bashfiles)
-	for idx in range(len(bashes)):
-		temp = template.replace("**path**", paths[idx])
+	with open("HomeRangeAnalysis_Automate_Submission.sh", "w") as writer:
+		for idx in range(len(bashes)):
+			temp = template.replace("**path**", paths[idx])
+			temp = temp.replace("**bashfile**", bashfiles[idx])
+			temp = temp.replace("**datapath**", os.path.abspath(args.Data))
+			writer.writelines(temp)
+
 	return
 
 if __name__ == '__main__':
