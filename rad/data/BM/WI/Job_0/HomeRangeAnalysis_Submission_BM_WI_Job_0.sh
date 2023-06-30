1453834a7b94efc17ed99ba8a23add218798920d
Ruben Dorador <rubendorador@gmail.com>
2023-06-30 10:00:41 -0400

    mapply method update2

--- rad/data/BM/WI/Job_0/HomeRangeAnalysis_Submission_BM_WI_Job_0.sh
+++ rad/data/BM/WI/Job_0/HomeRangeAnalysis_Submission_BM_WI_Job_0.sh
@@ -4,6 +4,8 @@
 #SBATCH --job-name=HomeRangeAnalysis_BM_WI_JOB_0
 #SBATCH --output=%x.out
 #SBATCH --comment=Caribou_Quintette
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
 
 module load r/4.2.2 udunits/2.2.28 gdal geos proj/9.0.1 protobuf
 
--- rad/templates/HomeRangeAnalysis_Submission_Template.sh
+++ rad/templates/HomeRangeAnalysis_Submission_Template.sh
@@ -4,6 +4,8 @@
 #SBATCH --job-name=HomeRangeAnalysis_**SA**_**SEASON**_JOB_**JOBID**
 #SBATCH --output=%x.out
 #SBATCH --comment=**PROJECT**
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
 
 module load r/4.2.2 udunits/2.2.28 gdal geos proj/9.0.1 protobuf
 
