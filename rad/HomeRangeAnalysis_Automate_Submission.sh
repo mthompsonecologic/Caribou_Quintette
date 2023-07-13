22936953d05949cfe2229ff8b9a7fdfcf289e184
Ruben Dorador <rubendorador@gmail.com>
2023-07-12 22:11:34 -0600

    updated automate

--- rad/HomeRangeAnalysis_Automate_Submission.sh
+++ rad/HomeRangeAnalysis_Automate_Submission.sh
@@ -1,160 +1,160 @@
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/CA/Job_1
+cd ~/scratch/sat-caribou/data/BM/CA/Job_1
 sbatch HomeRangeAnalysis_Submission_BM_CA_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/CA/Job_0
+cd ~/scratch/sat-caribou/data/BM/CA/Job_0
 sbatch HomeRangeAnalysis_Submission_BM_CA_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/CA/Job_2
+cd ~/scratch/sat-caribou/data/BM/CA/Job_2
 sbatch HomeRangeAnalysis_Submission_BM_CA_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/CA/Job_3
+cd ~/scratch/sat-caribou/data/BM/CA/Job_3
 sbatch HomeRangeAnalysis_Submission_BM_CA_Job_3.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/CA/Job_4
+cd ~/scratch/sat-caribou/data/BM/CA/Job_4
 sbatch HomeRangeAnalysis_Submission_BM_CA_Job_4.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/RU/Job_1
+cd ~/scratch/sat-caribou/data/BM/RU/Job_1
 sbatch HomeRangeAnalysis_Submission_BM_RU_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/RU/Job_0
+cd ~/scratch/sat-caribou/data/BM/RU/Job_0
 sbatch HomeRangeAnalysis_Submission_BM_RU_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/RU/Job_2
+cd ~/scratch/sat-caribou/data/BM/RU/Job_2
 sbatch HomeRangeAnalysis_Submission_BM_RU_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/RU/Job_3
+cd ~/scratch/sat-caribou/data/BM/RU/Job_3
 sbatch HomeRangeAnalysis_Submission_BM_RU_Job_3.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/RU/Job_4
+cd ~/scratch/sat-caribou/data/BM/RU/Job_4
 sbatch HomeRangeAnalysis_Submission_BM_RU_Job_4.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/SU/Job_1
+cd ~/scratch/sat-caribou/data/BM/SU/Job_1
 sbatch HomeRangeAnalysis_Submission_BM_SU_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/SU/Job_0
+cd ~/scratch/sat-caribou/data/BM/SU/Job_0
 sbatch HomeRangeAnalysis_Submission_BM_SU_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/SU/Job_2
+cd ~/scratch/sat-caribou/data/BM/SU/Job_2
 sbatch HomeRangeAnalysis_Submission_BM_SU_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/SU/Job_3
+cd ~/scratch/sat-caribou/data/BM/SU/Job_3
 sbatch HomeRangeAnalysis_Submission_BM_SU_Job_3.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/SU/Job_4
+cd ~/scratch/sat-caribou/data/BM/SU/Job_4
 sbatch HomeRangeAnalysis_Submission_BM_SU_Job_4.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/WI/Job_1
+cd ~/scratch/sat-caribou/data/BM/WI/Job_1
 sbatch HomeRangeAnalysis_Submission_BM_WI_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/WI/Job_0
+cd ~/scratch/sat-caribou/data/BM/WI/Job_0
 sbatch HomeRangeAnalysis_Submission_BM_WI_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/WI/Job_2
+cd ~/scratch/sat-caribou/data/BM/WI/Job_2
 sbatch HomeRangeAnalysis_Submission_BM_WI_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/WI/Job_3
+cd ~/scratch/sat-caribou/data/BM/WI/Job_3
 sbatch HomeRangeAnalysis_Submission_BM_WI_Job_3.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/BM/WI/Job_4
+cd ~/scratch/sat-caribou/data/BM/WI/Job_4
 sbatch HomeRangeAnalysis_Submission_BM_WI_Job_4.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/CA/Job_1
+cd ~/scratch/sat-caribou/data/TR/CA/Job_1
 sbatch HomeRangeAnalysis_Submission_TR_CA_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/CA/Job_0
+cd ~/scratch/sat-caribou/data/TR/CA/Job_0
 sbatch HomeRangeAnalysis_Submission_TR_CA_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/CA/Job_2
+cd ~/scratch/sat-caribou/data/TR/CA/Job_2
 sbatch HomeRangeAnalysis_Submission_TR_CA_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/RU/Job_1
+cd ~/scratch/sat-caribou/data/TR/RU/Job_1
 sbatch HomeRangeAnalysis_Submission_TR_RU_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/RU/Job_0
+cd ~/scratch/sat-caribou/data/TR/RU/Job_0
 sbatch HomeRangeAnalysis_Submission_TR_RU_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/RU/Job_2
+cd ~/scratch/sat-caribou/data/TR/RU/Job_2
 sbatch HomeRangeAnalysis_Submission_TR_RU_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/SU/Job_1
+cd ~/scratch/sat-caribou/data/TR/SU/Job_1
 sbatch HomeRangeAnalysis_Submission_TR_SU_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/SU/Job_0
+cd ~/scratch/sat-caribou/data/TR/SU/Job_0
 sbatch HomeRangeAnalysis_Submission_TR_SU_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/SU/Job_2
+cd ~/scratch/sat-caribou/data/TR/SU/Job_2
 sbatch HomeRangeAnalysis_Submission_TR_SU_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/WI/Job_1
+cd ~/scratch/sat-caribou/data/TR/WI/Job_1
 sbatch HomeRangeAnalysis_Submission_TR_WI_Job_1.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/WI/Job_0
+cd ~/scratch/sat-caribou/data/TR/WI/Job_0
 sbatch HomeRangeAnalysis_Submission_TR_WI_Job_0.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data/TR/WI/Job_2
+cd ~/scratch/sat-caribou/data/TR/WI/Job_2
 sbatch HomeRangeAnalysis_Submission_TR_WI_Job_2.sh
-cd /Users/rubendorador/Github/Caribou_Quintette/rad/data
+cd ~/scratch/sat-caribou/data
 
 
