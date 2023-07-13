ce84862555b39ee665577992cd928c006a52ebaf
Ruben Dorador <rubendorador@gmail.com>
2023-07-12 22:23:07 -0600

    recreadted data

---
+++ rad/data/TR/WI/Job_0/HomeRangeAnalysis_Submission_TR_WI_Job_0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mem=64000M
+#SBATCH --time=71:00:00 # 48 hours
+#SBATCH --job-name=HomeRangeAnalysis_TR_WI_JOB_0
+#SBATCH --output=%x.out
+#SBATCH --comment=Caribou_Quintette
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+
+module load r/4.2.2 udunits/2.2.28 gdal geos proj/9.0.1 protobuf
+
+Rscript HomeRangeAnalysis_TR_WI_Job_0.R
